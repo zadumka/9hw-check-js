3ae5d449ca13fc6a7c3a66da34afd98e5b4c483d
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-27 18:10:46 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,59 +1,44 @@
-'use strict';
-
-const form = document.querySelector('.feedback-form');
-const labels = document.querySelectorAll('.feedback-form label');
-const emailInput = form.querySelector('input[name="email"]');
-const messageTextarea = form.querySelector('textarea[name="message"]');
-const button = document.querySelector('button');
-
-labels.forEach(label => {
-    label.classList.add('form-label');
-});
-emailInput.classList.add('form-input');
-messageTextarea.classList.add('form-textarea');
-button.classList.add('form-button');
-
-// =======================================
-
-let formData = {
+const formData = {
     email: "",
-    message: ""
-};
-
-// Збереження formData в локальне сховище
-function addToLocalStorage() {
-    localStorage.setItem('formText', JSON.stringify(formData));
+    message: "",
 }
 
-// Заповнення форми
-function fillFromLocalStorage() {
-    const savedData = localStorage.getItem('formText');
-    if (savedData) {
-        formData = JSON.parse(savedData);
-        emailInput.value = formData.email;
-        messageTextarea.value = formData.message;
+const form = document.querySelector(".feedback-form");
+const formEmail = form.elements.email;
+const formMessage = form.elements.message;
+const key = "feedback-form-state";
+
+form.addEventListener("input", (event) => {
+    const { name, value } = event.target;
+    if (name === "email" || name === "message") {
+      formData[name] = value.trim();
+      localStorage.setItem(key, JSON.stringify(formData));
+    }
+  });
+
+function localData() {
+    const storage = localStorage.getItem(key);
+    if (storage) {
+        const parsedData = JSON.parse(storage);
+        formData.email = parsedData.email || "";
+        formData.message = parsedData.message || "";
+        formEmail.value = formData.email;
+        formMessage.value = formData.message;
     }
 }
 
-// Обробка input
-form.addEventListener('input', (event) => {
-    formData[event.target.name] = event.target.value;
-    addToLocalStorage();
-});
+localData();
 
-// Обробка submit
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
     if (!formData.email || !formData.message) {
-        alert('Fill please all fields');
-    } else {
-     
-        localStorage.removeItem('formText');
-        formData = { email: "", message: "" };
-        form.reset();
+        alert("Fill please all fields");
+        return;
     }
-});
+    console.log(formData);   
 
-// Заповнення при завантаженні
-window.addEventListener('load', fillFromLocalStorage);
+    localStorage.removeItem(key);
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+})
